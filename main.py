a15bdf45017b43a048465d2cd1b53285d10b5332
saltedsky <a.huang.011@gmail.com>
2015-04-21 17:32:13 -0700

    made classes

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import pygame
+from Sprite import *
+from Background import *
 
 def main():
     """ Main game loop lives here. Also loading pictures and stuff.
     """
     pygame.init()
-    #dimensions of screen
-    screen = pygame.display.set_mode((1000, 395))
 
-    #Loads player image and gets rid of background color (white)
-    player_imageR = pygame.image.load('spear1.png').convert()
-    player_imageL = pygame.image.load('spear1L.png').convert()
-    player_imageR.set_colorkey((255,255,255))
-    player_imageL.set_colorkey((255,255,255))
+    screen_width = 1000
+    screen_height = 395
+    screen = pygame.display.set_mode((screen_width, screen_height))  #dimensions of screen
 
-    #loads background image
-    background_image = pygame.image.load('background.png').convert()
+    #creating stuff
+    sprite = Sprite()
+    background = Background()
 
     done = False
-
-    #Position of sprite
-    x = 0
-    y = 275
 
     #times stuff
     clock = pygame.time.Clock()
 
     #Player default direction
-    default_player = player_imageR
-
+    default_player = sprite.imageR
 
     #Text 
     font = pygame.font.Font("Fipps-Regular.otf", 30)
@@ -41,39 +35,41 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
 
-            #Changes color of block 
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                is_blue = not is_blue
-
         #erases past junk
-        screen.fill((0,0,0))
+    	screen.fill((0,0,0))
         
         #Copy background to screen and puts in default player
-        screen.blit(background_image, [0,0])
-        screen.blit(default_player, [x,y])
+        screen.blit(background.image, [background.x,background.y])
+        screen.blit(default_player, [sprite.x,sprite.y])
 
         #Text Stuff
-        screen.blit(text,( 0, 0))
+        screen.blit(text,(0, 0))
         
         #Tests for key presses and loads images
         pressed = pygame.key.get_pressed()
-        
-        #if pressed[pygame.K_UP]:
+
+        if pressed[pygame.K_UP]:
+            pass
 
         #if pressed[pygame.K_DOWN]: y  += 3
+
+        if pressed[pygame.K_LEFT] and sprite.x>0:
+            default_player = sprite.imageL
+            if sprite.x < 200 or bX > -200:
+            	sprite.x -= 3
+            elif background.x < 200: 
+            	background.x += 3
+
+        if pressed[pygame.K_RIGHT] and sprite.x<900:
+            default_player = sprite.imageR
+            if sprite.x < 200:
+            	sprite.x += 3
+            elif background.x > screen_width - background.width:
+            	background.x -= 3
+            #move_sound.play()
+
         
-        if pressed[pygame.K_LEFT] and 0<x:
-            default_player = player_imageL
-            x  -= 3
-            
-        if pressed[pygame.K_RIGHT] and x<900:
-            default_player = player_imageR
-            x  += 3
-            move_sound.play()
-
-
-        #Waits 1/60 second
-        clock.tick(60)
+        clock.tick(24) #waits 1/24 second
         
         pygame.display.flip()
 
@@ -88,11 +84,11 @@ def walktimer():
     """
 
 def jump():
-    """ Makes spirte jump when up key is pressed
+    """ Makes sprite jump when up key is pressed
     """
+
 
     
 
 if __name__ == "__main__":   # did we just RUN this file?
    main()                   # if so, we call main()
-
