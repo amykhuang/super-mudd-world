baee0af7857ab2018654e8fd1e2df35e7bf78785
saltedsky <a.huang.011@gmail.com>
2015-04-29 16:08:16 -0700

    Update Background.py

--- Background.py
+++ Background.py
@@ -1,12 +1,23 @@
 import pygame
 
 class Background:
+
+	SCREEN_WIDTH = 1000
+	SCREEN_HEIGHT = 395
+	BOTTOM_MARGIN = 20
+
 	def __init__(self):
 		#load image
-		self.image = pygame.image.load('background2.png').convert()
+		self.image = pygame.image.load('background-1500.png').convert()
 		self.width = self.image.get_width()
 		self.height = self.image.get_height()
+		self.title = "SUPER MUDD"
 		
 		#position
 		self.x = 0
 		self.y = 0
+
+		#text 
+		self.font = pygame.font.Font("Fipps-Regular.otf", 30)
+		self.text = self.font.render(self.title, True, (0,0,0))
+		
