053225bf8352939df33a5c0c9b9eef14dc5fbea3
saltedsky <a.huang.011@gmail.com>
2015-05-15 17:51:35 -0700

    added collisions

--- Background.py
+++ Background.py
@@ -5,6 +5,7 @@ class Background(pygame.sprite.Sprite):
 	SCREEN_WIDTH = 1000
 	SCREEN_HEIGHT = 395
 	BOTTOM_MARGIN = 20
+	STEP = 3
 
 	def __init__(self):
 		#load image
@@ -12,9 +13,11 @@ class Background(pygame.sprite.Sprite):
 		self.width = self.image.get_width()
 		self.height = self.image.get_height()
 		self.rect = self.image.get_rect()
-		self.world_shift = 0
 		self.title = "SUPER MUDD"
+
+		#adding the platforms
 		self.platforms = []
+		#might put these positions in a separate file at some point
 		self.plat_positions = [
 			[100, 300],
 			[300, 200],
@@ -23,28 +26,26 @@ class Background(pygame.sprite.Sprite):
 		]
 		self.make_Platforms()
 		
-		#position
+		#position and velocity
 		self.rect.x = 0
 		self.rect.y = 0
-
 		self.vel = [0,0]
 
 		#text 
 		self.font = pygame.font.Font("Fipps-Regular.otf", 30)
 		self.text = self.font.render(self.title, True, (0,0,0))
 
+	#writing to the screen
 	def blit(self, screen):
-
 		screen.blit(self.image, [self.rect.x, self.rect.y])
 		screen.blit(self.text, (0,0))
 		for i in self.platforms:
 			i.blit(screen)
 
-	def collision_check(self, player):
+	def collision_check_h(self, player):
 		collided = ""
 		kill = False
 
-		# See if we hit anything
 		block_hit_list = pygame.sprite.spritecollide(player, self.platforms, False)
 		for block in block_hit_list:
 			if player.vel[0] > 0:
@@ -54,55 +55,52 @@ class Background(pygame.sprite.Sprite):
 				player.rect.left = block.rect.right
 				collided += "left"
 
+		return collided, kill
+
+	def collision_check_v(self, player):
 		collided = ""
+		kill = False
+
 		block_hit_list = pygame.sprite.spritecollide(player, self.platforms, False)
 		for block in block_hit_list:
 			# Reset our position based on the top/bottom of the object.
 			if player.vel[1] > 0:
-				player.rect.bottom = block.rect.top - 10
+				player.rect.bottom = block.rect.top
 				collided += "bottom"
 			elif player.vel[1] < 0:
-				player.rect.top = block.rect.bottom + 10
+				player.rect.top = block.rect.bottom
 				collided += "top"
-			player.vel[1] = 0
+			player.vel[1] = 0	#resetting to 0 so it doesn't keep moving
+
 		return collided, kill
 
 
 	def make_Platforms(self):
+		self.platforms += [Platform(0, Background.SCREEN_HEIGHT-Background.BOTTOM_MARGIN, "ground")]
 		for i in self.plat_positions:
-			new_plat = Platform(i[0],i[1])
+			new_plat = Platform(i[0],i[1], "platform")
 			self.platforms += [new_plat]
 
-	def shift_world(self, shift_x):
-		"""when the user moves and we need to shift everything"""
-		#the shift amount
-		self.vel[0] += shift_x
-
-		#shift all other things
+	def shift_world(self, shift):
+		self.rect.left -= shift
 		for platform in self.platforms:
-			platform.vel[0] += shift_x
-
-	def update(self, dt, collides):
-		self.rect.x += self.vel[0] * dt
-		self.rect.y += self.vel[1] * dt
-
-		for platform in self.platforms:
-			platform.rect.x += self.vel[0] * dt
-			platform.rect.y += self.vel[1] * dt
+			platform.rect.left -= shift
 
 class Platform:
-	def __init__(self, x, y):
-		self.image = pygame.image.load('redplat.png').convert()
+	def __init__(self, x, y, typ):
+		if typ == "platform":
+			self.image = pygame.image.load('redplat.png').convert()
+		else:
+			self.image = pygame.image.load('ground.png').convert_alpha()
+
 		self.height = self.image.get_height()
 		self.width = self.image.get_width()
-
 		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
-
+		self.rect.left = x
+		self.rect.top = y
 		self.vel = [0,0]
+		self.type = typ
 
-		self.type = "platform"
 
 	def blit(self, screen):
-		screen.blit(self.image, [self.rect.x,self.rect.y])
+		screen.blit(self.image, [self.rect.left, self.rect.top])
