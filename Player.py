271cba71b3ad452ab14258ab2ab368a4b721d285
saltedsky <a.huang.011@gmail.com>
2015-06-01 23:07:14 -0700

    added walking animations

--- Player.py
+++ Player.py
@@ -1,18 +1,27 @@
 import pygame
 from Background import Background as bg
 
+WHITE = (255,255,255)
+
 class Player(pygame.sprite.Sprite):
 	GRAVITY = 1000
 
 	def __init__(self):
 		#loading images
-		self.imageR = pygame.image.load('spear1.png').convert()
-		self.imageL = pygame.image.load('spear1L.png').convert()
-		self.default_image = self.imageR
+		self.imageR = pygame.image.load('images/spear1.png').convert()
+		self.imageR1 = pygame.image.load('images/exbig1.png').convert()
+		self.imageR2 = pygame.image.load('images/exbig2.png').convert()
+		self.imageR3 = pygame.image.load('images/exbig3.png').convert()
+		self.imageL = pygame.image.load('images/spear1L.png').convert()
+		self.default_image = self.imageR1
+
+		player_images = [self.imageR, self.imageR1, self.imageR2, self.imageR3, self.imageL]
+
 		self.height = self.imageR.get_height()
 		self.width = self.imageR.get_width()
 		self.rect = self.imageR.get_rect()
-		self.move_sound = pygame.mixer.Sound('smb_jumpsmall.wav')
+
+		self.move_sound = pygame.mixer.Sound('sounds/smb_jumpsmall.wav')
 		self.font = pygame.font.Font("Fipps-Regular.otf", 30)
 
 		self.state = "standing"
@@ -28,8 +37,8 @@ class Player(pygame.sprite.Sprite):
 		self.vel = [0,0]
 		
 		#making white parts black
-		self.imageR.set_colorkey((255,255,255))
-		self.imageL.set_colorkey((255,255,255))
+		for image in player_images:
+			image.set_colorkey(WHITE)
 
 	#writing to the screen
 	def blit(self, screen):
@@ -76,17 +85,21 @@ class Player(pygame.sprite.Sprite):
 		self.gravity(dt)
 		self.vel[0] = 0
 
+		if self.health <= 0:
+			return "gameover"		#sets game_over to True
+		return "in_game"
+
 	def move(self, dx, dy, background, dt, ticks):
 		""" moves the Player
 		"""
 		if self.vel[0] < 0 and self.rect.x > 0:	#move left
-			self.default_image = self.imageL
+			self.walkcycle("left", ticks)
 			if self.rect.x < 200 or background.rect.x > -10:
 				self.rect.left += dx
 			elif background.rect.x < 200:
 				background.shift_world(dx)
 		if self.vel[0] > 0 and self.rect.x < 900:	#move right
-			self.default_image = self.imageR
+			self.walkcycle("right", ticks)
 			if self.rect.x < 200:
 				self.rect.left += dx
 			elif background.rect.x > bg.SCREEN_WIDTH - background.width:
@@ -118,13 +131,28 @@ class Player(pygame.sprite.Sprite):
 			self.damaged = True
 			return ticks
 		else:
-			if ticks - self.damage_time > 3000:
+			if ticks - self.damage_time > 300:
 				self.damaged = False
 			return self.damage_time
 
-	def walkcycle():
+	def walkcycle(self, direction, ticks):
 	    """ creates the walking animation
 	    """
+	    if direction == "left":
+	    	if ticks % 30 <= 10:
+	    		self.default_image = self.imageL
+	    	elif ticks % 30 <= 20 and ticks % 30 > 10:
+	    		self.default_image = self.imageL
+	    	else:
+	    		self.default_image = self.imageL
+	    elif direction == "right":
+	    	if ticks % 30 <= 10:
+	    		self.default_image = self.imageR1
+	    	elif ticks % 30 <= 20 and ticks % 30 > 10:
+	    		self.default_image = self.imageR2
+	    	else:
+	    		self.default_image = self.imageR3
+
 
 	def gravity(self, dt):
 		self.vel[1] += Player.GRAVITY * dt
