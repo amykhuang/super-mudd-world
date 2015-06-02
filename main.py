1c62017ff30b065bea502247b4707a804503c34a
saltedsky <a.huang.011@gmail.com>
2015-06-02 00:07:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import pygame
 from Player import Player
 from Background import Background
+import resources as R
 
 def main():
     pygame.init()
 
     #dimensions of screen
-    screen = pygame.display.set_mode((Background.SCREEN_WIDTH, Background.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((R.SCREEN_WIDTH, R.SCREEN_HEIGHT))
     pygame.display.set_caption("Super Mudd World")
 
     #creating entities
     player = Player()
     background = Background()
+
+    #states: in_game, gameover
+    state = "in_game"
 
     done = False
 
@@ -20,19 +24,36 @@ def main():
     fps = 30 #time step        
     
     while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        if state == "in_game":
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+            
+            screen.fill((0,0,0))    #erases past junk
+
+            background.blit(screen) #blit things to screen
+            player.blit(screen)
+
+            state = player.update(1.0/fps, background)   #player update
+
+        elif state == "gameover":
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+
+            background.endscreen(screen)
+
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_q]:
                 done = True
-        
-        screen.fill((0,0,0))    #erases past junk
-
-        background.blit(screen) #blit things to screen
-        player.blit(screen)
-
-        player.update(1.0/fps, background)   #player update
+            elif pressed[pygame.K_SPACE]:
+                state = "in_game"
+                player = Player()
+                background = Background()
 
         clock.tick(fps)
         pygame.display.flip()
+
 
     pygame.quit()
 
