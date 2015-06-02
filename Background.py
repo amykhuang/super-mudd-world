2b4dcac3c191011fb28898cca1cfb8346adb8b0a
saltedsky <a.huang.011@gmail.com>
2015-06-02 00:08:52 -0700

    change image file locations

--- Background.py
+++ Background.py
@@ -1,15 +1,11 @@
 import pygame
+import resources as R
 
 class Background(pygame.sprite.Sprite):
 
-	SCREEN_WIDTH = 1000
-	SCREEN_HEIGHT = 395
-	BOTTOM_MARGIN = 20
-	STEP = 3
-
 	def __init__(self):
 		#load image
-		self.image = pygame.image.load('images/background-1500.png').convert()
+		self.image = R.IMAGES['background-1500.png'].convert()
 		self.width = self.image.get_width()
 		self.height = self.image.get_height()
 		self.rect = self.image.get_rect()
@@ -32,34 +28,33 @@ class Background(pygame.sprite.Sprite):
 		self.vel = [0,0]
 
 		#text 
-		self.font = pygame.font.Font("Fipps-Regular.otf", 30)
-		self.smallfont = pygame.font.Font("Fipps-Regular.otf", 20)
-		self.text = self.font.render(self.title, True, (0,0,0))
+		self.smallfont = pygame.font.Font("data/fonts/Fipps-Regular.otf", 20)
+		self.text = R.FONTS['Fipps-Regular.otf'].render(self.title, True, (0,0,0))
 
 	#writing to the screen
 	def blit(self, screen):
 		screen.blit(self.image, [self.rect.x, self.rect.y])
-		screen.blit(self.text, (0,0))
+		screen.blit(self.text, (2,0))
 		for i in self.platforms:
 			i.blit(screen)
 
 	def endscreen(self, screen):
 		screen.fill((0,0,0))
-		title = self.font.render("Game Over", True, (100,0,0))
+		title = R.FONTS['Fipps-Regular.otf'].render("Game Over", True, (100,0,0))
 		title_rect = title.get_rect()
-		title_x = screen.get_width()/2 - title_rect.width/2
-		title_y = screen.get_height()/2 - title_rect.height/2 - 20
+		title_x = R.SCREEN_HEIGHT/2 - title_rect.width/2
+		title_y = R.SCREEN_HEIGHT/2 - title_rect.height/2 - 20
 		subtitle = self.smallfont.render("Press space to play again. Press q to quit.", True, (100,0,0))
 		subtitle_rect = subtitle.get_rect()
-		subtitle_x = screen.get_width()/2 - subtitle_rect.width/2
-		subtitle_y = screen.get_height()/2 - subtitle_rect.height/2 + 70
+		subtitle_x = R.SCREEN_WIDTH/2 - subtitle_rect.width/2
+		subtitle_y = R.SCREEN_WIDTH/2 - subtitle_rect.height/2 + 70
 
 		screen.blit(title, [title_x, title_y])
 		screen.blit(subtitle, [subtitle_x, subtitle_y])
 
 
 	def make_Platforms(self):
-		self.platforms += [Platform(0, Background.SCREEN_HEIGHT-Background.BOTTOM_MARGIN, "ground")]
+		self.platforms += [Platform(0, R.SCREEN_HEIGHT-R.BOTTOM_MARGIN, "ground")]
 		for i in self.plat_positions:
 			new_plat = Platform(i[0],i[1],i[2])
 			self.platforms += [new_plat]
@@ -72,11 +67,11 @@ class Background(pygame.sprite.Sprite):
 class Platform:
 	def __init__(self, x, y, typ):
 		if typ == "platform":
-			self.image = pygame.image.load('images/redplat.png').convert()
+			self.image = R.IMAGES['redplat.png']
 		elif typ == "spike":
-			self.image = pygame.image.load('images/spikeplat.png').convert()
+			self.image = R.IMAGES['spikeplat.png']
 		else:
-			self.image = pygame.image.load('images/ground.png').convert_alpha()
+			self.image = R.IMAGES['ground.png']
 
 		self.height = self.image.get_height()
 		self.width = self.image.get_width()
