10f90e787bb3676a4dd238381bb6ab121dd03627
saltedsky <a.huang.011@gmail.com>
2015-06-24 12:45:55 -0700

    New Objects class!

---
+++ objects.py
@@ -0,0 +1,34 @@
+import pygame
+import resources as R
+
+class Object(pygame.sprite.Sprite):
+
+	def __init__(self, x, y, typ):
+		pygame.sprite.Sprite.__init__(self)
+
+		types = {
+			'cup': [R.IMAGES['cup.png'], False],
+			'glass': [R.IMAGES['glass.png'], True]
+		}
+
+		self.image = types[typ][0]
+
+		self.height = self.image.get_height()
+		self.width = self.image.get_width()
+		self.rect = self.image.get_rect()
+		
+		#position and velocity
+		self.rect.x = x
+		self.rect.y = y + 64 - self.height
+
+		self.type = "object"
+		self.deadly = types[typ][1]
+		self.species = typ
+		self.picked_up = False
+
+	def update(self, dt, bg_pos):
+		""" update the position of the enemy
+			so that it moves back and forth
+		"""
+		pass
+
